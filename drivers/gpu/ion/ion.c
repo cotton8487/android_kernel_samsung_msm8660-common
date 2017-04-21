BytecodeMe/vanquish

ion: add hack for IOMMU camera
liboemcamera requests memory from the CAMERA or IOMMU heaps, which
we cannot provide currently. Since we cannot change liboemcamera, remap
to CP_MM heap if CAMERA heap is requested
Adapted from HTC msm8960 kernel

Change-Id: I12b00799b6844d0d09f2c55ad19cf31e556e5bb5

drivers/gpu/ion/ion.c
@@ -419,6 +419,13 @@ struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
 			continue;
 		/* if the caller didn't specify this heap type */
 		if (!((1 << heap->id) & flags))
+#ifndef CONFIG_MSM_IOMMU
+		/* hack for IOMMU based liboemcamera,
+		* if CAMERA heap requested, use CP_MM heap
+		*/
+		if (!(heap->id == ION_CP_MM_HEAP_ID &&
+		((1 << ION_CAMERA_HEAP_ID) & flags)))
+#endif
 			continue;
 		/* Do not allow un-secure heap if secure is specified */
 		if (secure_allocation && (heap->type != ION_HEAP_TYPE_CP))

cotton8487/android_kernel_samsung_msm8660-common

drivers/gpu/ion/ion.c
@@ -430,6 +430,13 @@ struct ion_handle *ion_alloc(struct ion_client *client, size_t len,
 			continue;
 		/* if the caller didn't specify this heap type */
 		if (!((1 << heap->id) & heap_mask))
+#ifndef CONFIG_MSM_IOMMU
+		/* hack for IOMMU based liboemcamera,
+		* if CAMERA heap requested, use CP_MM heap
+		*/
+		if (!(heap->id == ION_CP_MM_HEAP_ID &&
+		((1 << ION_CAMERA_HEAP_ID) & heap_mask)))
+#endif
 			continue;
 		/* Do not allow un-secure heap if secure is specified */
 		if (secure_allocation &&